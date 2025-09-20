b038512cd088a90188ac791094fd21362901eb88
ketan <ketanghumare999@gmail.com>
2025-09-21 00:24:52 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -27,9 +27,17 @@ export default function Home() {
           </h2>
 
           <div className="mt-8 flex items-center gap-6">
-            <button className="rounded-full px-6 py-3 bg-black/90 text-white shadow-lg hover:scale-[1.01] transition transform">
-              Start Project
-            </button>
+            <a
+            href="#_"
+            className="relative inline-flex items-center justify-start px-6 py-2 overflow-hidden font-bold rounded-full group"
+          >
+            <span className="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+            <span className="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
+            <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-gray-900">
+              Get Started
+            </span>
+            <span className="absolute inset-0 border-2 border-white rounded-full"></span>
+          </a>
 
             
           </div>
